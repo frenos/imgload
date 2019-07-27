0cfa1ba54a556a71a0c77eee759a54aa222d5580
Frederic <frenos@users.noreply.github.com>
2019-07-27 17:37:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', "r") as readme_file:
 
 setup(
     name='imgload',
-    version='0.1',
+    version='0.2.0',
     description='An image downloading library',
     long_description=long_description,
     long_description_content_type="text/markdown",
